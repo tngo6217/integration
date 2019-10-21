521d37179bf79e1ec0db609dcef28f1cd5e2e0d8
tngo6217 <tnngo6217@eagle.fgcu.edu>
2019-10-20 23:35:28 -0400

    introduction to new functions

--- Main.py
+++ Main.py
@@ -26,7 +26,7 @@ print ("True value of x or y is:", x or y)
 print ("True value of not y is:", not y)
 print("Now let's figure out how the functions if...else, the while loops, and for loops work!")
 print("We're going to find the total of shopping items.")
-#
+# Exercise from Hackerrank.
 # The following code lines will ask user to input the price of the items, which are positive numbers.
 # The program will stop and output the total of all items when the user inputs numbers less than or equal to 0.
 total= 0
@@ -38,4 +38,35 @@ print ("The total of your items is: ", format(total, '.2f'))
 print("The next program code is to accept the cost of two items to be purchased, then the payment amount.")
 print("The output is going to be a thank you message with how much change will be given if the payment amount is greater than or equal to the amount of the 2 items.")
 print("If not, it would tell the user how much is still owed.")
+#Get the price of the first item
+price1 = float(input("Enter the cost of the first item: "))
+#Get the price of the second item
+price2 = float(input("Enter the cost of the second item: "))
+#Get the payment amount
+payment = float(input("Enter the payment amount: "))
+difference = payment - price1 - price2
+# Determine if customer still owes
+if difference<0:
+    print("You still owe ${:,.2f}".format(-difference))
+
+# or if change is owed
+else:
+    print("Thank you, your change is ${:,.2f}".format(difference))
+import random
+def getMessage(userNum,randNum):
+    if userNum == randNum:
+        print("You picked the same number as the computer!")
+    elif userNum>randNum:
+        print("Your number is higher than computer's number.")
+    elif userNum<randNum:
+        print("Your number is smaller than computer's number.")
+def main():
+    userNum= int(input("Enter a number betwwen 1 and 5: "))
+    while userNum>5 or userNum<1:
+        userNum= int(input("Invalid number. Please enter again: "))
+    randNum=random.randint(1,5)
+    print("Computer number:",randNum)
+    print("User number:",userNum)
+    getMessage(userNum,randNum)
+main()
 
