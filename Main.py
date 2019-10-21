211e79e161e52c227ba6c868a6827c8ab221fe6d
tngo6217 <tnngo6217@eagle.fgcu.edu>
2019-10-20 21:30:03 -0400

    introduction to new functions

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 #Programmer: Trinh Ngo
-#This program is to creat a quiz game
+#This program is to show some basic functions of Python 3
 
 first_last_name= input("Please enter your first and last name: ")
-print ("Hi"+" "+first_last_name+"! \nWelcome to Exciting Math Quiz.")
+print ("Hi"+" "+first_last_name+"! \nWelcome to Python Experience.")
 print ("The answer is going to be 58 if we type 'print(23+5*7)'")
 print (23+5*7)
 a= int(input("Enter the first number (it should be an integer): "))
@@ -24,7 +24,18 @@ print ("We are going to figure out the true value of x (True) and y (False)")
 print ("True value of x and y is:", x and y )
 print ("True value of x or y is:", x or y)
 print ("True value of not y is:", not y)
-print("Now let's figure out how the function if...else, the while loops, and for loops work")
-print("We're going to find the total of a shopping items")
-enter = int(input("Enter an integer number: "))
+print("Now let's figure out how the functions if...else, the while loops, and for loops work!")
+print("We're going to find the total of shopping items.")
+#
+# The following code lines will ask user to input the price of the items, which are positive numbers.
+# The program will stop and output the total of all items when the user inputs numbers less than or equal to 0.
+total= 0
+price= 1
+while price>0:
+    price = float(input("Please enter the price of the item: "))
+    total +=price
+print ("The total of your items is: ", format(total, '.2f'))
+print("The next program code is to accept the cost of two items to be purchased, then the payment amount.")
+print("The output is going to be a thank you message with how much change will be given if the payment amount is greater than or equal to the amount of the 2 items.")
+print("If not, it would tell the user how much is still owed.")
 
