3fa30dcc40fa7bc4546935b3aab6519f005d429d
tngo6217 <tnngo6217@eagle.fgcu.edu>
2019-11-24 23:51:32 -0500

    introduction to new functions

--- Main.py
+++ Main.py
@@ -9,11 +9,13 @@ print("The answer is going to be 58 if we type 'print(23+5*7)'")
 print(23 + 5 * 7)
 a = int(input("Enter the first number (it should be an integer): "))
 b = int(input("Enter the second number (it should be an integer): "))
-sum = int(a + b)
+"""Cannot create a function to make sure the user enter integer numbers 
+because we just introduce some basic functions in this part."""
+addition = (a + b)
 difference = (a - b)
 product = (a * b)
 quotient = (a / b)
-print("The sum of a+b is: ", sum)
+print("The sum of a+b is: ", addition)
 print("The difference of a-b is: ", difference)
 print("The product of a*b is: ", product)
 print("The quotient of a/b is:", quotient)
@@ -26,20 +28,24 @@ print("We are going to figure out the true value of x (True) and y (False)")
 print("True value of x and y is:", x and y)
 print("True value of x or y is:", x or y)
 print("True value of not y is:", not y)
-print("Now let's figure out how the functions if...else, the while loops, and "
-      "for loops work!")
-print("We're going to find the total of shopping items.")
 
 # Exercise from Hackerrank.
 # The following code lines will ask user to input the price of the items,
 # which are positive numbers.
 # The program will stop and output the total of all items when the user
 # inputs numbers less than or equal to 0.
+"""
+This file is to introduce while loops and for loops
+___author___= Trinh Ngo
+"""
+
 """ 
     While loop is used to iterate over a block of code as long as the test 
     expression (condition) is true.
 """
-
+print("Now let's figure out how the functions if...else, the while loops, and "
+      "for loops work!")
+print("We're going to find the total of shopping items.")
 total = 0
 price = 1
 while price > 0:
@@ -65,7 +71,7 @@ for numStudent in range(1, 4):
     studentName = input(
         "Please enter student " + str(numStudent) + "'s name: ")
     for x in range(1, 4):
-        score = int(input("Please enter score " + str(x) + ": "))
+        score = float(input("Please enter score " + str(x) + ": "))
         total = total + score
     average = total / 3
     print("Name: " + studentName)
@@ -94,6 +100,11 @@ if difference < 0:
 else:
     print("Thank you, your change is ${:,.2f}".format(difference))
 
+"""
+The file is to show how to create a new function.
+___authors__= Trinh Ngo
+"""
+
 
 def get_message(user_num, rand_num):
     """Define a new function:
@@ -118,40 +129,55 @@ def main():
     rand_num = random.randint(1, 5)
     print("Computer number:", )
     print("User number:", user_num)
+    """Return the created function get_message to compare the two numbers 
+    and output the result."""
     get_message(user_num, rand_num)
 
 
 main()
+"""Call the function main"""
 
 
 # This following function shows how parameters in a def function is changed to
 # arguments when the function is called.
 
+# noinspection PyShadowingNames
 def number(a):
     for a in range(0, 11, 2):
         a = str("This is an even number")
     return a
 
 
+# Allen Telson helped me with the following code
+
+
 def b():
     while True:
+        """The lines below is to ignore inputs that are not integer and ask 
+        the user to input again"""
         try:
             num1 = int(input("Please enter a random integer from 0 to 10: "))
             return num1
         except ValueError:
+            """Ignore value error"""
             print("This is not an integer.")
             continue
 
 
 def c(num1):
     while num1 < 0 or num1 > 10:
+        """Conditions for num1"""
         num1 = int(
             input("Your number is not between 0 and 10. Please try again: "))
-        if num1 % 2 == 0:
-            print(number(num1))
-        elif num1 % 2 == 1:
-            print("This is an odd number")
+    if num1 % 2 == 0:
+        """Recall the function number(a) and a is replaced by the argument 
+        num1"""
+        print(number(num1))
+    elif num1 % 2 == 1:
+        print("This is an odd number")
 
 
 result = b()
+"""Attach the function b() to result"""
 c(result)
+"""Call the function c() with the new argument."""
